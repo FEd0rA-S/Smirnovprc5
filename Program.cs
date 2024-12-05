0cd093d8d8f7c4752083df419497667b8de2d7bc
FEd0rA-S <rdxdcsgo@gmail.com>
2024-12-05 12:16:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+using BlazorApp1.Auth;
+using BlazorApp1.Components;
+using BlazorApp1.Models;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddAuthenticationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+            builder.Services.AddScoped<ProtectedSessionStorage>();
+            builder.Services.AddScoped<ProtectedLocalStorage>();
+
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddSingleton<LeemonContext>();
+
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+
+            
+
+
+        }
+
+    }
+
+}
